2acf275a38b3bb8afa35787737f7cc49084d81a2
gutenmorgenuhu <uhu@areaemig.de>
2017-04-03 13:00:47 +0200

    Different Alphabets

--- src/sample/BruteForce.java
+++ src/sample/BruteForce.java
@@ -6,26 +6,48 @@ package sample;
 public class BruteForce implements Runnable{
     Controller controller;
     String word;
+    String alphabet;
 
-
-    public BruteForce(Controller controller, String word){
+    public BruteForce(Controller controller, String word, String alphabet) {
         this.controller = controller;
         this.word = word;
+        this.alphabet = alphabet;
     }
 
 
     @Override
     public void run() {
-        BruteForcer bruteForcer = BruteForcer.createAlphaBruteForcer();
+        BruteForcer bruteForcer;
+        switch (alphabet) {
+            case "alpha":
+                bruteForcer = BruteForcer.createAlphaBruteForcer();
+                break;
+            case "numeric":
+                bruteForcer = BruteForcer.createNumericBruteForcer();
+                break;
+            case "alphanumeric":
+                bruteForcer = BruteForcer.createAlphaNumericBruteForcer();
+                break;
+            default:
+                bruteForcer = BruteForcer.createAlphaNumericBruteForcer();
+                break;
+        }
+
+
         String bfGenPW =null;
+        int schritte = 100;
+        int counter = 0;
         while (!word.equals(bfGenPW)) {
             bfGenPW = bruteForcer.computeNextCombination();
+            if (counter % schritte == 0)
 
-            controller.calculations++;
+                controller.calculations++;
+            counter++;
 
         }
         System.out.println(String.format("Decrypted string: %s", bfGenPW));
         controller.stopTimer();
         controller.refreshDisplay();
+        controller.refreshTextField(bfGenPW);
     }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,22 +3,44 @@ package sample;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.LinkedList;
 
 public class Controller implements Runnable{
-    @FXML private TextField textFieldPassword;
-    @FXML private Label labelCalcPerSecond;
-    @FXML private Label labelDauer;
-    @FXML private Button buttonKnacken;
-    @FXML private ProgressIndicator progressIndicator;
-
     public Long calculations = new Long(0);
     public long startTime = System.nanoTime();
     public long stopTime = System.nanoTime();
     public Long runTime = 0L;
+    public double calcPerSecond = 0.0;
+    @FXML
+    private PasswordField textFieldPassword;
+    @FXML
+    private Label labelCalcs;
+    @FXML
+    private Label labelCalcsPerSecond;
+    @FXML
+    private Label labelDauer;
+    @FXML
+    private Button buttonKnacken;
+    @FXML
+    private ProgressIndicator progressIndicator;
+    @FXML
+    private RadioButton checkBoxAlpha;
+    @FXML
+    private RadioButton checkBoxNumeric;
+    @FXML
+    private RadioButton checkBoxAlphaNumeric;
+    @FXML
+    private TextArea textFieldResult;
+    private LinkedList<RadioButton> checkBoxes = new LinkedList<RadioButton>();
+
+    @FXML
+    public void initialize() {
+        checkBoxes.add(checkBoxAlpha);
+        checkBoxes.add(checkBoxNumeric);
+        checkBoxes.add(checkBoxAlphaNumeric);
+    }
 
    @FXML private void knackenKlicked(ActionEvent event){
        resetDisplay();
@@ -34,7 +56,7 @@ public class Controller implements Runnable{
         progressIndicator.setVisible(true);
         buttonKnacken.setDisable(true);
 
-       BruteForce b = new BruteForce(this,word);
+        BruteForce b = new BruteForce(this, word, checkedCheckBox());
        Thread t1 = new Thread(b);
        t1.start();
 
@@ -55,21 +77,44 @@ public class Controller implements Runnable{
 
     }
 
+    public void refreshTextField(String text) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                textFieldResult.setText(text + System.getProperty("line.separator"));
+            }
+        });
+    }
+
     private void resetDisplay(){
         calculations = 0L;
         startTime = 0L;
         stopTime = 0L;
         runTime = 0L;
-
-
     }
+
+    private String checkedCheckBox() {
+        String checkedBox = "";
+        for (RadioButton checkBox : checkBoxes) {
+            if (checkBox.isSelected()) {
+                checkedBox = checkBox.getId();
+            }
+        }
+        return checkedBox;
+    }
+
+
     @Override
     public void run() {
         runTime = stopTime-startTime;
         double seconds = (double)runTime / 1000000000.0;
         labelDauer.setText(String.valueOf(seconds));
-        labelCalcPerSecond.setText(calculations.toString());
+        labelCalcs.setText(calculations.toString());
         progressIndicator.setVisible(false);
         buttonKnacken.setDisable(false);
+        calcPerSecond = calculations / seconds;
+        labelCalcsPerSecond.setText(String.valueOf((long) calcPerSecond));
+
+
     }
 }

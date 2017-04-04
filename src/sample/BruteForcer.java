0dd4a3759a6f81ca3c38889a9768753b9f2b9546
gutenmorgenuhu <uhu@areaemig.de>
2017-04-04 20:55:02 +0200

    License

--- src/sample/BruteForcer.java
+++ src/sample/BruteForcer.java
@@ -1,15 +1,42 @@
 package sample;
 import java.util.Arrays;
 
+/**
+ * Copyright:
+ * https://github.com/a11n/bruteforce
+ * https://github.com/a11n
+ * <p>
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2015 André Diermann
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 public class BruteForcer {
+    private static final String NUMERIC_ALPHABET = "0123456789";
+    private static final String LOWER_CASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER_CASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private final char[] alphabet;
     private final int alphabetLength;
     private int[] indices;
     private char[] combination;
-
-    private static final String NUMERIC_ALPHABET = "0123456789";
-    private static final String LOWER_CASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    private static final String UPPER_CASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     private BruteForcer(String alphabet) {
         this.alphabet = alphabet.toCharArray();
@@ -19,15 +46,15 @@ public class BruteForcer {
         combination = new char[1];
     }
 
-    public static BruteForcer createNumericBruteForcer(){
+    static BruteForcer createNumericBruteForcer() {
         return new BruteForcer(NUMERIC_ALPHABET);
     }
 
-    public static BruteForcer createAlphaBruteForcer(){
+    static BruteForcer createAlphaBruteForcer() {
         return new BruteForcer(LOWER_CASE_ALPHABET + UPPER_CASE_ALPHABET);
     }
 
-    public static BruteForcer createAlphaNumericBruteForcer(){
+    static BruteForcer createAlphaNumericBruteForcer() {
         return new BruteForcer(LOWER_CASE_ALPHABET + UPPER_CASE_ALPHABET + NUMERIC_ALPHABET);
     }
 
@@ -36,7 +63,7 @@ public class BruteForcer {
         return new BruteForcer(alphabet);
     }
 
-    public String computeNextCombination() {
+    String computeNextCombination() {
         combination[0] = alphabet[indices[0]];
         String nextCombination = String.valueOf(combination);
 
